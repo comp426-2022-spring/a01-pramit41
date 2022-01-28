fb39e7dcd410080520d18557461e2f7c6e4da82c
Pramit Thapa <69735326+pramit41@users.noreply.github.com>
2022-01-27 21:17:10 -0500

    changed port

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // Require http module
 const http = require('http')
 
@@ -13,7 +12,7 @@ const args = require('minimist')(process.argv.slice(2))
 args['port']
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.PORT || process.env.port || 3000
+const port = args.port || process.env.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
